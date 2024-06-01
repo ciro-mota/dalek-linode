1de427e8f8078ecdd16b80dccbb08140332ff6a6
Ciro Mota <ciro.mota@gmail.com>
2024-06-01 16:04:18 -0300

    fix: Added print information about steps in execution

--- scripts/dalek-linode.sh
+++ scripts/dalek-linode.sh
@@ -6,7 +6,7 @@
 ## LICENSE:
 ###		GPLv3. <https://github.com/ciro-mota/dalek-linode/blob/main/LICENSE>
 ## CHANGELOG:
-### 	Last Update 26/10/2023. <https://github.com/ciro-mota/dalek-linode/commits/main>
+### 	Last Update 01/06/2024. <https://github.com/ciro-mota/dalek-linode/commits/main>
 
 set -e
 
@@ -20,6 +20,9 @@ tagName=("prod"
 	"PROD")
 
 ## Check, preserve and delete VMs:
+
+echo -e "\e[33;1mChecking for Linodes..\e[m"
+
 for idM in $listMachineIds; do
 	prodTagsM=$(linode-cli linodes view "$idM" --text --format tags | tail -n +2)
 	machineName=$(linode-cli linodes view "$idM" --text --format label | tail -n +2)
@@ -27,25 +30,28 @@ for idM in $listMachineIds; do
 
 	for listTags in "${tagName[@]}"; do
 		if [[ "$prodTagsM" == "$listTags"* ]]; then
-			echo "Preserving machine name: $machineName with tagged: $prodTagsM"
+			echo "Preserving machine name: '$machineName' with tagged: '$prodTagsM'"
 			matched=true
 			break
 		fi
 	done
 
 	if [[ "$machineName" == *lke* ]]; then
-			echo "Preserving machine name: $machineName part of LKE cluster."
+			echo "Preserving machine name: '$machineName' part of LKE cluster."
 			matched=true
 			break
 		fi
 
 	if [ "$matched" = false ]; then
-		echo "Deleting the machine with name: $machineName"
+		echo "Deleting the machine with name: '$machineName'"
 		linode-cli linodes delete --suppress-warnings "$idM"
 	fi
 done
 
 ## Check, preserve and delete Volumes:
+
+echo -e "\e[33;1mChecking for Volumes..\e[m"
+
 for idV in $listVolumesIds; do
 	prodTagsV=$(linode-cli volumes view "$idV" --text --format tags | tail -n +2)
 	volumeName=$(linode-cli volumes view "$idV" --text --format label | tail -n +2)
@@ -53,19 +59,22 @@ for idV in $listVolumesIds; do
 
 	for listTags in "${tagName[@]}"; do
 		if [[ "$prodTagsV" == "$listTags"* ]]; then
-			echo "Preserving volume name: $volumeName with tagged: $prodTagsV"
+			echo "Preserving volume name: '$volumeName' with tagged: '$prodTagsV'"
 			matched=true
 			break
 		fi
 	done
 
 	if [ "$matched" = false ]; then
-		echo "Deleting the volume with name: $volumeName"
+		echo "Deleting the volume with name: '$volumeName'"
 		linode-cli volumes delete --suppress-warnings "$idV"
 	fi
 done
 
 ## Check, preserve and delete Nodebalancers:
+
+echo -e "\e[33;1mChecking for Nodebalancers..\e[m"
+
 for idB in $listNodeBalancersIds; do
 	prodTagsNB=$(linode-cli nodebalancers view "$idB" --text --format tags | tail -n +2)
 	NBName=$(linode-cli nodebalancers view "$idB" --text --format label | tail -n +2)
@@ -73,19 +82,22 @@ for idB in $listNodeBalancersIds; do
 
 	for listTags in "${tagName[@]}"; do
 		if [[ "$prodTagsNB" == "$listTags"* ]]; then
-			echo "Preserving NodeBalancers name: $NBName with tagged: $prodTagsNB"
+			echo "Preserving NodeBalancers name: '$NBName' with tagged: '$prodTagsNB'"
 			matched=true
 			break
 		fi
 	done
 
 	if [ "$matched" = false ]; then
-		echo "Deleting the NodeBalancers with name: $NBName"
+		echo "Deleting the NodeBalancers with name: '$NBName'"
 		linode-cli nodebalancers delete --suppress-warnings "$idB"
 	fi
 done
 
 ## Check, preserve and delete Linode Kubernets:
+
+echo -e "\e[33;1mChecking for Clusters Kubernets..\e[m"
+
 for idL in $listLKEIds; do
 	prodTagsLKE=$(linode-cli lke cluster-view "$idL" --text --format tags | tail -n +2)
 	LKEName=$(linode-cli lke cluster-view "$idL" --text --format label | tail -n +2)
@@ -93,16 +105,16 @@ for idL in $listLKEIds; do
 
 	for listTags in "${tagName[@]}"; do
 		if [[ "$prodTagsLKE" == "$listTags"* ]]; then
-			echo "Preserving cluster name: $LKEName with tagged: $prodTagsLKE"
+			echo "Preserving cluster name: '$LKEName' with tagged: '$prodTagsLKE'"
 			matched=true
 			break
 		fi
 	done
 
 	if [ "$matched" = false ]; then
-		echo "Deleting the cluster with name: $LKEName"
+		echo "Deleting the cluster with name: '$LKEName'"
 		linode-cli lke cluster-delete --suppress-warnings "$idL"
 	fi
 done
 
-echo "Nothing more to do."
+echo -e "\e[32;1mNothing more to do. \xE2\x9C\x94 \e[m\n"
