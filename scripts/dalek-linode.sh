60c1b39ede751f93858bda25debee1e6b1872b8c
Ciro Mota <ciro.mota@gmail.com>
2025-09-03 16:10:20 -0300

    refactor: Compatibility with POSIX shells

--- scripts/dalek-linode.sh
+++ scripts/dalek-linode.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 ## AUTHOR:
-### 	Ciro Mota <contato.ciromota@outlook.com>
+### Ciro Mota <contato.ciromota@outlook.com>
 ## NAME:
-### 	dalek-linode.sh.
+### dalek-linode.sh.
 ## LICENSE:
-###		GPLv3. <https://github.com/ciro-mota/dalek-linode/blob/main/LICENSE>
+### GPLv3. <https://github.com/ciro-mota/dalek-linode/blob/main/LICENSE>
 ## CHANGELOG:
-### 	Last Update 16/05/2025. <https://github.com/ciro-mota/dalek-linode/commits/main>
+### Last Update 03/09/2025. <https://github.com/ciro-mota/dalek-linode/commits/main>
 
 set -e
 
@@ -15,108 +15,110 @@ listMachineIds=$(linode-cli linodes list --text --format "id" | tail -n +2)
 listVolumesIds=$(linode-cli volumes list --text --format "id" | tail -n +2)
 listNodeBalancersIds=$(linode-cli nodebalancers list --text --format "id" | tail -n +2)
 listLKEIds=$(linode-cli lke clusters-list --text --format "id" | tail -n +2)
-tagName=("prod"
-	"Production"
-	"PROD"
-	"prd"
-	"PRD")
+# POSIX compatible tag list (space-separated)
+tagName="prod Production PROD prd PRD"
 
 ## Check, preserve and delete VMs:
-
-echo -e "\e[33;1mChecking for Linodes..\e[m"
+printf "\033[33;1mChecking for Linodes..\033[0m\n"
 
 for idM in $listMachineIds; do
 	prodTagsM=$(linode-cli linodes view "$idM" --text --format tags | tail -n +2)
 	machineName=$(linode-cli linodes view "$idM" --text --format label | tail -n +2)
 	matched=false
 
-	for listTags in "${tagName[@]}"; do
-		if [[ "$prodTagsM" == "$listTags"* ]]; then
-			echo "Preserving machine name: '$machineName' with tagged: '$prodTagsM'"
-			matched=true
-			break
-		fi
+	for listTags in $tagName; do
+		case "$prodTagsM" in
+			"$listTags"*)
+				echo "Preserving machine name: '$machineName' with tagged: '$prodTagsM'"
+				matched=true
+				break
+				;;
+		esac
 	done
 
-	if [[ "$machineName" == *lke* ]]; then
+	case "$machineName" in
+		*lke*)
 			echo "Preserving machine name: '$machineName' part of LKE cluster."
 			matched=true
-			break
-		fi
+			;;
+	esac
 
-	if [ "$matched" = false ]; then
+	if [ "$matched" = "false" ]; then
 		echo "Deleting the machine with name: '$machineName'"
 		linode-cli linodes delete --suppress-warnings "$idM"
 	fi
 done
 
 ## Check, preserve and delete Volumes:
-
-echo -e "\e[33;1mChecking for Volumes..\e[m"
+printf "\033[33;1mChecking for Volumes..\033[0m\n"
 
 for idV in $listVolumesIds; do
 	prodTagsV=$(linode-cli volumes view "$idV" --text --format tags | tail -n +2)
 	volumeName=$(linode-cli volumes view "$idV" --text --format label | tail -n +2)
 	matched=false
 
-	for listTags in "${tagName[@]}"; do
-		if [[ "$prodTagsV" == "$listTags"* ]]; then
-			echo "Preserving volume name: '$volumeName' with tagged: '$prodTagsV'"
-			matched=true
-			break
-		fi
+	for listTags in $tagName; do
+		case "$prodTagsV" in
+			"$listTags"*)
+				echo "Preserving volume name: '$volumeName' with tagged: '$prodTagsV'"
+				matched=true
+				break
+				;;
+		esac
 	done
 
-	if [ "$matched" = false ]; then
+	if [ "$matched" = "false" ]; then
 		echo "Deleting the volume with name: '$volumeName'"
 		linode-cli volumes delete --suppress-warnings "$idV"
 	fi
 done
 
 ## Check, preserve and delete Nodebalancers:
-
-echo -e "\e[33;1mChecking for Nodebalancers..\e[m"
+printf "\033[33;1mChecking for Nodebalancers..\033[0m\n"
 
 for idB in $listNodeBalancersIds; do
 	prodTagsNB=$(linode-cli nodebalancers view "$idB" --text --format tags | tail -n +2)
 	NBName=$(linode-cli nodebalancers view "$idB" --text --format label | tail -n +2)
 	matched=false
 
-	for listTags in "${tagName[@]}"; do
-		if [[ "$prodTagsNB" == "$listTags"* ]]; then
-			echo "Preserving NodeBalancers name: '$NBName' with tagged: '$prodTagsNB'"
-			matched=true
-			break
-		fi
+	for listTags in $tagName; do
+		case "$prodTagsNB" in
+			"$listTags"*)
+				echo "Preserving NodeBalancers name: '$NBName' with tagged: '$prodTagsNB'"
+				matched=true
+				break
+				;;
+		esac
 	done
 
-	if [ "$matched" = false ]; then
+	if [ "$matched" = "false" ]; then
 		echo "Deleting the NodeBalancers with name: '$NBName'"
 		linode-cli nodebalancers delete --suppress-warnings "$idB"
 	fi
 done
 
 ## Check, preserve and delete Linode Kubernets:
-
-echo -e "\e[33;1mChecking for Clusters Kubernets..\e[m"
+printf "\033[33;1mChecking for Clusters Kubernets..\033[0m\n"
 
 for idL in $listLKEIds; do
 	prodTagsLKE=$(linode-cli lke cluster-view "$idL" --text --format tags | tail -n +2)
 	LKEName=$(linode-cli lke cluster-view "$idL" --text --format label | tail -n +2)
 	matched=false
 
-	for listTags in "${tagName[@]}"; do
-		if [[ "$prodTagsLKE" == "$listTags"* ]]; then
-			echo "Preserving cluster name: '$LKEName' with tagged: '$prodTagsLKE'"
-			matched=true
-			break
-		fi
+	for listTags in $tagName; do
+		case "$prodTagsLKE" in
+			"$listTags"*)
+				echo "Preserving cluster name: '$LKEName' with tagged: '$prodTagsLKE'"
+				matched=true
+				break
+				;;
+		esac
 	done
 
-	if [ "$matched" = false ]; then
+	if [ "$matched" = "false" ]; then
 		echo "Deleting the cluster with name: '$LKEName'"
 		linode-cli lke cluster-delete --suppress-warnings "$idL"
 	fi
 done
 
-echo -e "\e[32;1mNothing more to do. \xE2\x9C\x94 \e[m\n"
+printf "\033[32;1mNothing more to do. \xE2\x9C\x94 \033[0m\n\n"
